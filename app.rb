f59d8234aa7bbfffa87b22addbc036eef8cac717
大川雄生 <okawauki@OkawaUki.local>
2015-01-24 18:55:43 +0900

    Create routing for shaking counter

--- app.rb
+++ app.rb
@@ -29,6 +29,12 @@ get '/' do
   erb :index
 end
 
+get '/classes/count_action' do
+    hash = {:count_value => {"__op":"Increment","amount":1}}
+    path = "/2013-09-01/classes/shake_count/kBJKPWZhrXZ4mJF7"
+    put_request(path, hash)
+end
+
 get '/classes/start_action' do
     hash = {:flag_value => 1}
     path = "/2013-09-01/classes/slot_start_flag/Gg8weX5ZoD7X8WVn"
